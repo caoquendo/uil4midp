d2690faf1e04e7a049eae429c9338310970d2024
andres1905 <andres1905@6ae54fba-da03-4509-8369-9fe4cd14c483>
2010-11-21 04:24:28 +0000

    Añadidas clases basicas

---
+++ trunk/UIL4MIDP/src/ec/edu/epn/fis/uil4midp/components/VisualComponent.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ec.edu.epn.fis.uil4midp.components;
+
+/**
+ *
+ * @author Andrés
+ */
+public abstract class VisualComponent {
+
+}
---
+++ trunk/UIL4MIDP/src/ec/edu/epn/fis/uil4midp/components/containers/Container.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ec.edu.epn.fis.uil4midp.components.containers;
+
+import ec.edu.epn.fis.uil4midp.components.VisualComponent;
+
+/**
+ *
+ * @author Andrés
+ */
+public class Container extends VisualComponent {
+
+}
