093050300a110a105d9029eff38ca64e4380a259
andres1905 <andres1905@6ae54fba-da03-4509-8369-9fe4cd14c483>
2011-02-05 01:22:13 +0000

    Añadida lógica a TabsController

--- trunk/UIL4MIDP/src/ec/edu/epn/fis/uil4midp/controllers/NavigableController.java
+++ trunk/UIL4MIDP/src/ec/edu/epn/fis/uil4midp/controllers/NavigableController.java
@@ -3,10 +3,11 @@ package ec.edu.epn.fis.uil4midp.controllers;
 
 import ec.edu.epn.fis.uil4midp.views.AbstractView;
 import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Image;
 
 public class NavigableController implements IController {
 
-    public void addView(AbstractView view) {
+    public void addView(AbstractView view, Image icon) {
     }
 
     public void paint(Graphics graphics) {
